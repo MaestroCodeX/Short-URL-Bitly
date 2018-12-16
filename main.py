6c31e4ffd1b9132e8d02a872736cc28e7a886efc
asvirin <45921969+asvirin@users.noreply.github.com>
2018-12-16 21:56:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import os
+from dotenv import load_dotenv
+import argparse
+
+def get_link(long_url):
+    url_short = 'https://api-ssl.bitly.com/v4/bitlinks'
+    headers = {"Authorization": key}
+    body = {"long_url": long_url}
+    response = requests.post(url_short, json=body, headers=headers)
+    if response.ok:
+        data = response.json()
+        return(data['link'])
+    else:
+        return 
+
+def get_clicks(url):
+    url_clicks = 'https://api-ssl.bitly.com/v4/bitlinks/'+ url +'/clicks/summary'
+    headers = {"Authorization": key}
+    json = {'unit': 'day', 'units': '-1'}
+    response = requests.get(url_clicks, json=json, headers=headers)
+    if response.ok:
+        data = response.json()
+        return(data['total_clicks'])
+    else:
+        return(get_link(enter_url))
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Описание что делает программа')
+    parser.add_argument('url', help='Ссылка')
+    args = parser.parse_args()
+    enter_url = args.url
+    load_dotenv()
+    #key = os.getenv("key")
+    key = 'Bearer 54e0b10a39e63587b1470ada7c6046460dcc54da'
+    answer = get_clicks(enter_url)
+    if answer == None:
+        print('Ошибка запроса')
+    else:
+        print('Ответ: {}'.format(answer))

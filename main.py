1ac827496fdabede96a01152a05ba6d411bb1f92
asvirin <45921969+asvirin@users.noreply.github.com>
2018-12-19 14:28:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import os
 from dotenv import load_dotenv
 import argparse
 
-def get_link(long_url):
+def create_bitlink(long_url):
     url_short = 'https://api-ssl.bitly.com/v4/bitlinks'
     headers = {"Authorization": key}
     body = {"long_url": long_url}
     response = requests.post(url_short, json=body, headers=headers)
     if response.ok:
         data = response.json()
-        return(data['link'])
+        return data['link']
     else:
         return 
 
@@ -21,9 +21,9 @@ def get_clicks(url):
     response = requests.get(url_clicks, json=json, headers=headers)
     if response.ok:
         data = response.json()
-        return(data['total_clicks'])
+        return data['total_clicks']
     else:
-        return(get_link(enter_url))
+        return create_bitlink(enter_url)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Описание что делает программа')
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     enter_url = args.url
     load_dotenv()
-    key = os.getenv("key")
+    key = os.getenv("auth_key_bitlink")
     answer = get_clicks(enter_url)
     if answer == None:
         print('Ошибка запроса')
